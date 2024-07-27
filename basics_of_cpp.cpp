5ce9a90746cab094e70ea8a16a226f85cd1b5f82
Vaibhav Ranga <vaibhavranga2@gmail.com>
2024-07-28 00:21:48 +0530

    added some comments

---
+++ basics_of_cpp.cpp
@@ -0,0 +1,18 @@
+//include (a.k.a. pre-processor directive) means import and iostream is a header file that contains definition of cin, cout etc. 
+//But there are multiple definitions of cout and cin etc in iostream.
+//Namespace is a portion/ segment of code. We define that the definition of cout and cin that has to be used lies in the std (standard) namespace block.
+
+//bool      - 1
+//char      - 1 or 2 or 4
+//short     - 2
+//int       - 4
+//long      - 8
+//float     - 4
+//double    - 8
+
+
+#include<iostream>
+using namespace std;        
+
+int main() {   
+}
